58fa7c24a750eb4b5561249a29d6d808590f2caa
Mariano-CE <marianomm1301@outlook.com>
2018-08-30 17:11:07 -0600

    Primer Commit

---
+++ cmake-build-debug/CMakeFiles/Busquedas.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Busquedas.dir/main.cpp.o"
+  "Busquedas.pdb"
+  "Busquedas"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Busquedas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Busquedas.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Busquedas.
+# This may be replaced when dependencies are built.
